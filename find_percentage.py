e25c9fa7ee8d2b82f7d3fb59540ff585f4f9c439
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2021-09-09 15:23:25 +0800

    Update find_percentage.py

--- find_percentage.py
+++ find_percentage.py
@@ -2,9 +2,9 @@ if __name__ == '__main__':
     n = int(input())
     student_marks = {}
     for _ in range(n):
-        name, *line = input().split()
+        name, *line = input().split() #! 
         scores = list(map(float, line))
-        student_marks[name] = scores
+        student_marks[name] = scores #!
     query_name = input()
     scores = student_marks[query_name]
     avg = sum(scores)/len(scores)
